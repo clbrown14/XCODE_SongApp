22d5dcf7c24ecee8d1ddec112de9fefcf83212e1
Carl Brow <carlbrown4185@gmail.com>
2021-04-25 12:29:36 -0500

    Initial Commit

---
+++ Shared/XCODE_SongAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCODE_SongAppApp.swift
+//  Shared
+//
+//  Created by Carl Brown on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct XCODE_SongAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
